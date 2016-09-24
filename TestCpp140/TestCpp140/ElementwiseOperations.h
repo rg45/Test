06626e8912e87b3df142cd108d002df28b2e1709
rg45 <rg45.rom@gmail.com>
2016-09-25 00:05:23 +0300

    [Tests]: ElementwiseOperations.h

--- TestCpp140/TestCpp140/ElementwiseOperations.h
+++ TestCpp140/TestCpp140/ElementwiseOperations.h
@@ -9,8 +9,6 @@
 #include <boost/type_traits/remove_reference.hpp>
 #include <functional>
 #include <initializer_list>
-#include <iostream>
-#include <string>
 #include <tuple>
 #include <typeinfo>
 
@@ -55,55 +53,5 @@ template <typename, typename = void> struct IsRange_ : boost::mpl::false_ { };
 template <typename T> struct IsRange_<T, EnableIfRange<T>> : boost::mpl::true_ { };
 template <typename T> constexpr bool IsRange = IsRange_<T>::value;
 
-template <typename T> void testIsRange() { std::cout << IsRange<T> << std::endl; }
-template <typename T> void testIsRange(T&&) { testIsRange<T>(); }
-
-template <typename F, typename... Args>
-FunctionResult<F> testFunction(F&& f, Args&&... args)
-{
-   typedef FunctionSignature<F, Args...> Signature;
-   typedef std::function<Signature> Function;
-   Function g = std::forward<F>(f);
-   return g(std::forward<Args>(args)...);
-}
-
-void testString(const std::string& str)
-{
-   std::cout << str << std::endl;
-}
-
-void test()
-{
-   testString("Hello World!");
-
-   std::cout << std::boolalpha;
-
-   testIsRange("Hello!");
-   testIsRange(43);
-   testIsRange<const int(&&)[1]>();
-
-   auto λ = [](auto&&... args)
-   {
-      auto print = [](auto&& t) { std::cout << t << ' '; return 0; };
-      std::initializer_list<int>{ print(args)... };
-      return sizeof...(args);
-   };
-   std::cout << "argCount = " << testFunction(λ, "Pi =") << std::endl;
-   std::cout << "argCount = " << testFunction(λ, "Pi =", 3.14) << std::endl;
-   std::cout << "argCount = " << testFunction(λ, "Pi =", 3.14, "g =") << std::endl;
-   std::cout << "argCount = " << testFunction(λ, "Pi =", 3.14, "g =", 9.8) << std::endl;
-}
-
 } // namespace detail
-
-void test()
-{
-   detail::test();
-}
-
 } // namespace ElementwiseOperations
-
-void TestElementwiseOperations()
-{
-   ElementwiseOperations::test();
-}
