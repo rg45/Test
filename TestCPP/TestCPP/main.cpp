8070044147a99980f67c474f3da61e43349fbe3f
rg45 <rg45.rom@gmail.com>
2016-11-06 14:01:28 +0200

    fixed

--- TestCPP/TestCPP/main.cpp
+++ TestCPP/TestCPP/main.cpp
@@ -14,12 +14,10 @@ template <typename T> using Decay = typename std::decay<T>::type;
 template <typename T, typename = decltype(&T::operator())> using EnableIfFunctionObject = EnableIf<true>;
 template <typename T, typename = void> struct IsFunctionObject_ : std::false_type { };
 template <typename T> struct IsFunctionObject_<T, EnableIfFunctionObject<T>> : std::true_type { };
-template <typename T> constexpr bool isFunctionObject() { return IsFunctionObject_<Decay<T>>::value; };
-template <typename T> constexpr bool isFunctionObject(T&&) { return isFunctionObject<T>(); };
 
 template <typename T, typename = void> struct SignatureOf_;
 template <typename T>
-struct SignatureOf_<T, EnableIf<isFunctionObject<T>()>> { using type = int; };
+struct SignatureOf_<T, EnableIf<IsFunctionObject_<T>::value>> { using type = int; };
 
 template <typename T> using SignatureOf = typename SignatureOf_<T>::type;
 
@@ -27,10 +25,9 @@ template <typename T> using SignatureOf = typename SignatureOf_<T>::type;
 template <typename F, typename...Args>
 decltype(auto) smartcall(F&& f, Args&&...args)
 {
-   PRINT(isFunctionObject(f));
-   PRINT(isFunctionObject<F>());
+   PRINT(IsFunctionObject_<F>::value);
    PRINT(sizeof(&F::operator()));
-   PRINT(sizeof(EnableIf<isFunctionObject<F>()>*));
+   PRINT(sizeof(EnableIf<IsFunctionObject_<F>::value>*));
    PRINT(sizeof(typename SignatureOf_<F>::type)); // use of undefined type
 }
 
