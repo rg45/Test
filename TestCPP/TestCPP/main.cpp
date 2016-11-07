1af0c0d12f45e1b5137a086c5f8a20b8b68e082c
rg45 <rg45.rom@gmail.com>
2016-11-07 21:21:06 +0200

    [ScenarioHelpers]: GetTypeName

--- TestCPP/TestCPP/main.cpp
+++ TestCPP/TestCPP/main.cpp
@@ -1,19 +1,30 @@
 #include "stdafx.h"
 
-#include <iostream>
-#include <string>
 #include <functional>
+#include <iostream>
+#include <sstream>
+#include <string>
 
 #include <boost/noncopyable.hpp>
-
-// Development/Debug utility
-#define PRINT(ex) (std::cout << #ex" = " << (ex) << std::endl)
-template <typename T> void printType() { PRINT(__FUNCSIG__); }
-template <typename T> void printType(T&&) { printType<T>(); }
 
 // Will come with C++17
 template <bool cond, typename type = void> using enable_if_t = typename std::enable_if<cond, type>::type;
 template <typename T> using decay_t = typename std::decay<T>::type;
+
+// Development/Debug utility
+#define PRINT(ex) (std::cout << #ex" = " << (ex) << std::endl)
+
+template <typename T>
+std::string GetTypeName()
+{
+   std::ostringstream output;
+   output << __FUNCSIG__;
+   auto&& str = output.str();
+   auto begin = str.find(__FUNCTION__) + std::string(__FUNCTION__).size() + 1;
+   auto end = str.find_last_of('>');
+   return str.substr(begin, end - begin);
+}
+template <typename T> std::string GetTypeName(T&&) { return GetTypeName<T>(); }
 
 template <typename T, typename Head, typename...Tail>
 enable_if_t<!std::is_convertible<Head&&, T>::value, T&&> select(Head&&, Tail&&...tail)
@@ -35,13 +46,30 @@ T&& select()
 
 int main()
 {
-   void test_select(); test_select();
+   PRINT(GetTypeName(3.14));
+   PRINT(GetTypeName(std::forward<double&&>(3.14)));
+   PRINT(GetTypeName<decltype(3.14)>());
+   PRINT(GetTypeName<decltype(std::forward<double>(3.14))>());
+
+   double d = 3.14;
+   PRINT(GetTypeName(d));
+   PRINT(GetTypeName([](int){}));
+
+   auto createLambda = [](int x) { return [x](int y, auto&&...) { return x + y; }; };
+
+   PRINT(GetTypeName(createLambda(1)));
+   PRINT(GetTypeName(createLambda(2)));
+   PRINT(GetTypeName(&decltype(createLambda(2))::operator()<>));
+   PRINT(GetTypeName(&decltype(createLambda(2))::operator()<double>));
+   PRINT(GetTypeName(&decltype(createLambda(2))::operator()<double&>));
+
+
+   //void test_select(); test_select();
 
 
 //   auto l2 = [](double d, auto&&...context) { PRINT(d); PRINT(select<double&>(context...)); PRINT(sizeof...(context)); };
 //   l2(1.23, "Hello", true, 3.14);
 }
-
 void test_select()
 {
    auto d = 3.14;
