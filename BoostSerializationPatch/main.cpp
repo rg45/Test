769e88cd9e0cdd8851f63379b267ffe10f739d9c
rg45 <rg45.rom@gmail.com>
2016-11-18 17:15:26 +0200

    [BoostSerializationPatch]: just created the project

---
+++ BoostSerializationPatch/main.cpp
@@ -0,0 +1,9 @@
+#include "stdafx.h"
+
+#include <iostream>
+
+int main()
+{
+   PRINT(__FUNCSIG__);
+}
+
---
+++ BoostSerializationPatch/stdafx.h
@@ -0,0 +1,4 @@
+#pragma once
+
+// Development/Debug utility
+#define PRINT(ex) (std::cout << #ex" = " << (ex) << std::endl)
